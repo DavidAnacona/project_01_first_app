adafdd805a008968f89a23e7e08caa9a798e721d
DavidAnacona <danaconac@ucentral.edu.co>
2024-06-27 07:57:58 -0500

    my first component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyFirstComponent = () => {
+  return (
+    <>
+        <div>David Santiago Anacona Castellanos</div>
+        <div>27 de junio del 2024</div>
+    </>
+  )
+}
+
+export default MyFirstComponent
